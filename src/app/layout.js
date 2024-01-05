9ac1bacccbfbe2097c9e76e0d2167f6d690520e6
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-01-05 18:25:40 +0200

    done

---
+++ src/app/layout.js
@@ -0,0 +1,23 @@
+import { Roboto } from 'next/font/google'
+import './globals.css'
+
+const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] })
+
+export const metadata = {
+  title: 'CrustCraft',
+  description: `CrustCraft: Where Artisanal Pies Meet Culinary Mastery. 
+  Experience the perfection of handcrafted crusts and gourmet toppings in 
+  every bite. A symphony of flavors awaits at your favorite pizza haven.`,
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>
+        <main className='max-w-6xl mx-auto'>
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
