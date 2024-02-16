e216f358f05344f9561d6970d640373e0012abd6
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-02-16 17:11:17 +0200

    menu items page done

---
+++ src/app/menu-items/page.js
@@ -0,0 +1,35 @@
+'use client'
+import UserTabs from '../../components/layout/UserTabs';
+import { useProfile } from '../../components/UseProfile';
+
+export default function MenuItemsPage() {
+
+    const {loading, data} = useProfile();
+
+    if (loading) {
+        return 'Loading user info...';
+    }
+
+    if (!data.admin) {
+        return 'Not an admin.';
+    }
+
+    return (
+        <section className="mt-8">
+            <UserTabs isAdmin={true}/>
+            <form
+            className='mt-8 max-w-md mx-auto'
+            >
+                <div className='flex items-end gap-2'>
+                    <div className='grow'>
+                        <label>Menu item name</label>
+                        <input type="text"/>
+                    </div>
+                    <div>
+                        <button className='mb-2' type="submit">Create</button>
+                    </div>
+                </div>
+            </form>
+        </section>
+    )
+}
