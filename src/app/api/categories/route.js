71a55f3a342baaf4991c278c50ad653d000bfcc5
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-02-16 15:31:25 +0200

    done

---
+++ src/app/api/categories/route.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { Category } from "../../../models/Category";
+
+export async function POST(req) {
+    mongoose.connect(process.env.MONGO_URL);
+    const {name} = await req.json();
+    const categoryDoc = await Category.create({name});
+    return Response.json(categoryDoc);
+}
+
+export async function GET() {
+    mongoose.connect(process.env.MONGO_URL);
+    return Response.json(
+        await Category.find()
+    )
+}
