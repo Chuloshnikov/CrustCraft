e7e835d379e7ff719bb3b7e4aeb709c056d79109
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-03-08 17:00:25 +0200

    done

--- src/components/layout/MenuItemPriceProps.jsx
+++ src/components/layout/MenuItemPriceProps.jsx
@@ -1,9 +1,12 @@
 import {useState} from 'react'
 import { BsPlusLg } from "react-icons/bs";
 import { PiTrash } from "react-icons/pi";
+import { IoChevronUp } from "react-icons/io5";
+import { IoChevronDownSharp } from "react-icons/io5";
 
 const MenuItemPriceProps = ({name, titleLabel, priceLabel, addLabel, props, setProps}) => {
 
+    const [isOpen, setIsOpen] = useState(false);
 
     function addProp() {
         setProps(oldProps => {
@@ -28,49 +31,71 @@ const MenuItemPriceProps = ({name, titleLabel, priceLabel, addLabel, props, setP
   return (
     <div
     className='bg-gray-200 p-2 rounded-md mb-2'
+    >   
+    <div
+    className='flex justify-between mb-1'
     >
-        <label>{name}</label>
-        {props?.length > 0 && props.map((size, index) => (
-            <div
-            key={index}
-            className='flex items-end gap-2'
-            >
-                <div>
-                    <label>{titleLabel}</label>
-                    <input 
-                    onChange={e => editProp(e, index, 'name')}
-                    type="text" 
-                    placeholder="Size name" 
-                    value={size.name}
-                    />
-                </div>
-                <div>
-                    <label>{priceLabel}</label>
-                    <input
-                    onChange={e => editProp(e, index, 'price')} 
-                    type="text" 
-                    placeholder='Extra price' 
-                    value={size.price}
-                    />
-                </div>
-                <div>
-                    <button 
-                    onClick={() => removeProp(index)}
-                    type='button' 
-                    className='bg-white mb-[9px] px-2'>
-                        <PiTrash className='w-6 h-6'/>
-                    </button>
-                </div>
-            </div>
-        ))}
-        <button 
-        type='button'
-        onClick={addProp}
-        className='bg-white flex gap-1 items-center'
+        <div
+        className='flex gap-1'
         >
-            <BsPlusLg className='w-4 h-4'/>
-            <span>{addLabel}</span>
-        </button>
+            <h3 className='text-gray-700 text-base font-semibold'>{name}</h3>
+            <span>({props.length})</span>
+        </div>
+        <div>
+            <button 
+            onClick={() => setIsOpen(prev => !prev)}
+            className='bg-white' 
+            type="button"
+            >
+                {isOpen ? (<IoChevronUp/>) : (<IoChevronDownSharp/>)}
+            </button>
+        </div>
+    </div>
+        <div
+        className={isOpen ? 'block' : 'hidden'}
+        >
+            {props?.length > 0 && props.map((size, index) => (
+                <div
+                key={index}
+                className='flex items-end gap-2'
+                >
+                    <div>
+                        <label>{titleLabel}</label>
+                        <input 
+                        onChange={e => editProp(e, index, 'name')}
+                        type="text" 
+                        placeholder="Size name" 
+                        value={size.name}
+                        />
+                    </div>
+                    <div>
+                        <label>{priceLabel}</label>
+                        <input
+                        onChange={e => editProp(e, index, 'price')} 
+                        type="text" 
+                        placeholder='Extra price' 
+                        value={size.price}
+                        />
+                    </div>
+                    <div>
+                        <button 
+                        onClick={() => removeProp(index)}
+                        type='button' 
+                        className='bg-white mb-[9px] px-2'>
+                            <PiTrash className='w-6 h-6'/>
+                        </button>
+                    </div>
+                </div>
+            ))}
+            <button 
+            type='button'
+            onClick={addProp}
+            className='bg-white flex gap-1 items-center'
+            >
+                <BsPlusLg className='w-4 h-4'/>
+                <span>{addLabel}</span>
+            </button>
+        </div>  
     </div>
   )
 }
