e702abe3480dff86629f0d33b19fc19fd46927e9
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-03-18 19:16:39 +0200

    done

--- src/components/AppContext.js
+++ src/components/AppContext.js
@@ -1,17 +1,32 @@
 "use client"
 import {SessionProvider} from "next-auth/react";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext({});
 
 export function AppProvider ({ children }) {
-
     const [cartProducts, setCartProducts] = useState([]);
+    const ls = typeof window !== 'undefined' ? window.localStorage : null;
+
+    useEffect(() => {
+        if (ls && ls.getItem('cart')) {
+            setCartProducts( JSON.parse( ls.getItem('cart')));
+        }
+
+    }, []);
+
+    function saveCartToLocalStorage() {
+        if (ls) {
+            ls.setItem('cart', JSON.stringify(cartProducts));
+        }
+    }
+
 
     function addToCart(product, size = null, extras = []) {
         setCartProducts(prevProducts => {
             const cartProduct = {...product, size, extras};
             const newProducts = [...prevProducts, cartProduct];
+            saveCartToLocalStorage(newProducts);
             return newProducts;
         });
     }
