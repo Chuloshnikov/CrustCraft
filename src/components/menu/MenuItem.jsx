2dd8983f75747209c54591d38c181646f4874b79
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-03-20 15:41:53 +0200

    done

--- src/components/menu/MenuItem.jsx
+++ src/components/menu/MenuItem.jsx
@@ -26,33 +26,54 @@ const MenuItem = (menuItem) => {
     <>
     {showPopup && (
       <div className='fixed inset-0 bg-black/80 flex items-center justify-center'>
-        <div className='bg-white p-4 rounded-lg max-w-md'>
-          <Image 
-          src={image} 
-          alt={name} 
-          width={300} 
-          height={200}
-          className='mx-auto'
-          />
-          <h2 className='text-lg bold text-center'>{name}</h2>
-          <p className='text-center text-gray-600 text-sm mb-2'>{description}</p>
-          {sizes?.length > 0 && (
-            <div className='bg-gray-200 rounded-md p-2'>
-              <h3 className='text-center trxt-gray-500'>Pick your size</h3>
-              <div className='p-2'>
-                {sizes?.map((size, index) => (
-                    <label key={index} className='py-1 border flex gap-2'>
-                        <input 
-                        className='focus:ring-primary text-primary dark:focus:ring-gray-500 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 focus:border-white ring-offset-0'
-                        type="radio"
-                        /> 
-                        <span>{size.name}</span> 
-                        <span>${basePrice + size.price}</span>
-                    </label>
-                ))}
-              </div>
-            </div>  
-          )}
+        <div className='bg-white p-2 rounded-lg max-w-md '>
+          <div className='overflow-y-scroll p-2' style={{maxHeight: 'calc(100vh - 100px'}}>
+              <Image 
+              src={image} 
+              alt={name} 
+              width={300} 
+              height={200}
+              className='mx-auto'
+              />
+              <h2 className='text-lg font-bold text-center'>{name}</h2>
+              <p className='text-center text-gray-600 text-sm mb-2'>{description}</p>
+              {sizes?.length > 0 && (
+                <div className='bg-gray-200 rounded-md p-2'>
+                  <h3 className='text-center text-gray-500'>Pick your size</h3>
+                  <div className='py-2'>
+                    {sizes?.map((size, index) => (
+                        <label key={index} className='py-1 border flex gap-2'>
+                            <input 
+                            className='focus:ring-primary text-white focus:text-primary dark:focus:ring-gray-500 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 focus:border-white ring-offset-0'
+                            type="radio"
+                            /> 
+                            <span>{size.name}</span> 
+                            <span>${basePrice + size.price}</span>
+                        </label>
+                    ))}
+                  </div>
+                </div>  
+              )}
+              {sizes?.length > 0 && (
+              <div className='bg-gray-200 rounded-md p-2 mt-2'>
+                  <h3 className='text-center text-gray-500'>Pick your extra</h3>
+                  <div className='py-2'>
+                    {extraIngredientPrices?.map((extra, index) => (
+                        <label key={index} className='py-1 border flex gap-2'>
+                            <input 
+                            className='focus:ring-primary text-primary focus:text-primary dark:focus:ring-gray-500 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 focus:border-white ring-offset-0'
+                            type="checkbox"
+                            name={extra.name}
+                            /> 
+                            <span>{extra.name}</span> 
+                            <span>${extra.price}</span>
+                        </label>
+                    ))}
+                  </div>
+                </div>  
+                )}
+                <button className='mt-2 bg-primary text-white' type="button">Add to cart "selected price"</button>
+          </div>
         </div>
       </div>
     )}
