412ab19cc3981a583aecad7edb9bbbce36bf6b18
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-03-22 16:52:14 +0200

    done

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -56,9 +56,9 @@ export default function CartPage() {
                     {cartProducts?.length > 0 && cartProducts.map((product, index ) => (
                         <div
                         key={index}
-                        className="flex gap-4 mb-4 items-center gap-4 mb-4 border-b py-2 md:py-4"
+                        className="flex gap-4 mb-4 items-center gap-4 mb-4 border-b py-2"
                         >
-                            <div className="w-24">
+                            <div className="w-20">
                                 <Image width={240} height={240} src={product.image} alt={"product image"}/>
                             </div>
                             <div className="text-gray-700 grow">
