db1100f5d15168c413cfaf26b5410b326af3fa66
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-04-01 15:31:38 +0300

    done

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     images: {
       remotePatterns: [
         {
@@ -12,6 +13,9 @@ const nextConfig = {
         },
       ]
     },
+    typescript: {
+      ignoreBuildErrors: true,
+    }
 }
 
 module.exports = nextConfig
